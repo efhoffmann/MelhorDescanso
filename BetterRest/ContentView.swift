22b4e9365178350d3ac860d1b6b41f118452997b
Eduardo <eduhoffmann.sistemas@yahoo.com.br>
2023-05-26 10:40:33 -0300

    melhorias desafio

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,19 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-
+    
     @State var animateCoin = false
     @Environment(\.colorScheme) var colorScheme
-
     
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     @State private var image = "relogio"
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
-    @State private var showingAlert = false
+    @State private var showingAlert = true
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -34,94 +33,78 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                RadialGradient(gradient: Gradient(colors: [.green, .white]), center: .center, startRadius: 20, endRadius: 200)
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                ], center: .top, startRadius: 200, endRadius: 400).opacity(0.9)
+                    .ignoresSafeArea()
                 Form {
                     
-                    Text("Qual horário você quer acordar?")
-                        .font(.headline)
-                    
-                    DatePicker("Por favor, escolha a hora", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                    
-                    VStack(alignment: .leading, spacing: 0) {
+                    Section {
                         
-                        Text("Quantidade de sono desejável")
-                            .font(.headline)
+                        DatePicker("Por favor, escolha a hora", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
                         
+                    } header: {
+                        Text("Qual horário você quer acordar?")
+                            .titleStyle()
+                           
+                    }
+                    
+                    Section() {
+
                         Stepper("  \(sleepAmount.formatted()) horas", value: $sleepAmount, in: 4...12, step: 0.25)
-                    }
-                    
-                    
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("Ingestão diária de café")
-                            .font(.headline)
-                        
-                        Stepper(coffeeAmount == 1 ? "  1 xícara" : "  \(coffeeAmount) xícaras", value: $coffeeAmount, in: 1...20)
-                    }
-              
-            }
-                
-                // .background(Color(.systemGroupedBackground)).opacity(0.4)
-                .background(Color.green).opacity(0.5)
-                .scrollContentBackground(.hidden)
-            
-           
-            .navigationTitle("MelhorDescanso")
-            .toolbar {
-                Button("Calcular", action: calculateBedtime)
-                
-                    .alert(alertTitle, isPresented: $showingAlert) {
-                        Button("OK") { self.animateCoin.toggle()}
-                    } message: {
-                        Text(alertMessage)
-                    }
-            }
-                
-                VStack {
-                   
-                    Rectangle()
-                        .foregroundColor(colorScheme == .dark ? .white : .black)
-                        .frame(width: 150, height: 150, alignment: .center)
-                        .offset(y: 0)
-                        .mask(Image(systemName: "deskclock")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .rotation3DEffect(
-                                Angle(degrees: self.animateCoin ? 360 : 0),
-                                axis: (x: 0, y: self.animateCoin ? 360 : 0, z: 0)
-                                
-                            )
-                        )
-                        .offset(y: self.animateCoin ? 600 : 0)
-                        .animation(.linear(duration: 1))
-                   
-                }
-                
-                
-                VStack {
-                    (Image(image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 210.0, height: 110.0)
-                        .clipShape(Circle())
-                        .rotation3DEffect(
-                            Angle(degrees: self.animateCoin ? 360 : 0),
-                            axis: (x: 0, y: self.animateCoin ? 360 : 0, z: 0)
                             
-                        )
-                    )
-                    .offset(y: self.animateCoin ? 600 : 0)
-                    .animation(.linear(duration: 1))
+                    } header: {
+                        Text("Quantidade de sono desejável")
+                            .titleStyle()
+                            
+                    }
+                    // VStack(alignment: .leading, spacing: 0) {
+                    Section() {
+                        
+                        Picker("Xícaras ingeridas", selection: $coffeeAmount) {
+                            ForEach(1..<21) { item in
+                                Text((item == 1 ? "1 xícara" : "\(item) xícaras"))
+                            }
+                        }
+                        
+                    } header: {
+                        Text("Ingestão diária de café")
+                            .titleStyle()
+
+                    }
+                    Section() {
+                        HStack() {
+                            (Image(image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 210.0, height: 110.0, alignment: .center)
+                                .offset(y: 0)
+                                .clipShape(Circle()))
+                            
+                            Text("\(calculateBedtime())")
+                                .font(.system(size: 30, weight: .bold, design: .monospaced))
+                                .foregroundColor(Color.green)
+                        }
+                     
+                    } header: {
+                        Text("A hora ideal para você dormir é...")
+                            .font(.system(size: 14, weight: .bold, design: .monospaced))
+                            .foregroundColor(Color.orange)
+                    }
+                }
+                // .background(Color(.systemGroupedBackground)).opacity(0.4)
+               // .background(Color.green).opacity(0.5)
+                .scrollContentBackground(.hidden)
+                .navigationTitle("MelhorDescanso")
+                    
                 }
                 
+            }
+            
         }
-           
-          
-    }
-       
-    }
     
-    func calculateBedtime() {
+func calculateBedtime() -> String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -132,18 +115,36 @@ struct ContentView: View {
             
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "A hora ideal para você dormir é..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-            
+          
+           let date = sleepTime.formatted(date: .omitted, time: .shortened)
+            return date
         } catch {
-            alertTitle = "Erro"
+           
             alertMessage = "Desculpe, houve um problema ao calcular sua hora de dormir"
+            showingAlert = true
+           
         }
-        showingAlert = true
-        self.animateCoin.toggle()
-       
+    return ""
+      
+        
     }
 }
+
+    struct Title: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .font(.system(size: 16, weight: .bold, design: .monospaced))
+                .foregroundColor(Color.orange)
+            }
+        }
+
+    extension View {
+        func titleStyle() -> some View {
+            self.modifier(Title())
+        }
+    }
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
